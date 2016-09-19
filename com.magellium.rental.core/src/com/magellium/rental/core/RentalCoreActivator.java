955d752e8f42cef0898820d1c001728dff537cf8
jgr <jgr@PC003.magellium.com>
2016-09-19 17:40:31 +0200

    WindowBuilder
    Change-Id: I1d837ce5e0f0f2dbec085e627be29cdb8ea24e1a
    Signed-off-by: jgr <jgr@PC003.magellium.com>

---
+++ com.magellium.rental.core/src/com/magellium/rental/core/RentalCoreActivator.java
@@ -0,0 +1,37 @@
+package com.magellium.rental.core;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import com.opcoach.training.rental.RentalAgency;
+import com.opcoach.training.rental.helpers.RentalAgencyGenerator;
+
+public class RentalCoreActivator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		RentalCoreActivator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		RentalCoreActivator.context = null;
+	}
+	
+	public static RentalAgency getAgency () {
+		return RentalAgencyGenerator.createSampleAgency();
+	}
+
+}
