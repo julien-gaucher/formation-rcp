9bc95e3e1a6326fc8ac48cc782db5ff27691db96
jgr <jgr@PC003.magellium.com>
2016-09-20 17:11:33 +0200

    Perspective
    Change-Id: Id3e6f9f4ac89350a3f434df0906fbb407d9e3384
    Signed-off-by: jgr <jgr@PC003.magellium.com>

---
+++ com.magellium.rental.ui/src/perspective/RentalPerspective.java
@@ -0,0 +1,39 @@
+package perspective;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class RentalPerspective implements IPerspectiveFactory {
+
+	/**
+	 * Creates the initial layout for a page.
+	 */
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		String editorArea = layout.getEditorArea();
+		addFastViews(layout);
+		addViewShortcuts(layout);
+		addPerspectiveShortcuts(layout);
+		layout.addView("com.magellium.rental.ui.view.rental", IPageLayout.LEFT, 0.5f, IPageLayout.ID_EDITOR_AREA);
+		layout.addView("com.magellium.rental.ui.view.rental.agency", IPageLayout.LEFT, 0.5f, "com.magellium.rental.ui.view.rental");
+	}
+
+	/**
+	 * Add fast views to the perspective.
+	 */
+	private void addFastViews(IPageLayout layout) {
+	}
+
+	/**
+	 * Add view shortcuts to the perspective.
+	 */
+	private void addViewShortcuts(IPageLayout layout) {
+	}
+
+	/**
+	 * Add perspective shortcuts to the perspective.
+	 */
+	private void addPerspectiveShortcuts(IPageLayout layout) {
+	}
+
+}
